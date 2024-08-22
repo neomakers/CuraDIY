9e304637890d0fcb192381e8e15d1ed5bf223a25
Longer <77575048+neomakers@users.noreply.github.com>
2024-08-22 09:08:12 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,85 @@
-# CuraDIY
+For documentation check: https://github.com/daid/Cura/wiki  
+如需查看文档，请访问：https://github.com/daid/Cura/wiki
+
+For downloads check: https://github.com/daid/Cura/downloads  
+如需下载，请访问：https://github.com/daid/Cura/downloads
+
+This package includes two programs:  
+此软件包包含两个程序：
+
+Pronterface:  
+Pronterface：
+
+    An application for both manually controlling and automatically feeding gcode to a 3D printer.  
+    一个用于手动控制和自动向3D打印机传送GCode的应用程序。
+
+Cura:  
+
+   A easy to use program for slicing STL files using SkeinForge. Cura can also visualize the 3D models in a variety of ways.  
+   一个使用SkeinForge切片STL文件的易用程序。Cura还可以通过多种方式可视化3D模型。
+
+   On first run, Cura will ask to go through a set of calibration steps that will perform a series of operations on your 3D printer. One of those steps involves extruding a bit of filament where the filament is initially nearly fully extracted.  As such, it you will probably need to run Pronterface first, heat up the extruder enough to be able to extract the filament and then use the Pronterface interface to reverse the extruder motor until the filament is at the right position (specifically, until the filament end is even with where the Bowden tube leaves the extuder motor assembly, on an Ultimaker).  
+   第一次运行时，Cura会要求进行一系列校准步骤，这些步骤会对您的3D打印机执行一系列操作。其中一步是挤出一些几乎完全抽出的细丝。因此，您可能需要先运行Pronterface，将挤出机加热到足以抽出细丝的程度，然后使用Pronterface界面反向挤出机电机，直到细丝到达正确的位置（特别是，直到细丝末端与Bowden管离开挤出机电机组件的位置齐平，对于Ultimaker）。
+
+========  
+BUILDING  
+构建  
+========  
+
+    ./package.sh  
+
+The build script defaults to building for Windows.  If you want to build for Mac OS X or Linux, choose one of:  
+构建脚本默认构建Windows版本。如果您想构建Mac OS X或Linux版本，请选择以下之一：
+
+    ./package.sh osx64  
+    ./package.sh linux  
+
+Note that Mac OS X currently requires the manual installation of wxPython, PySerial, and PyOpenGL:  
+请注意，Mac OS X目前需要手动安装wxPython、PySerial和PyOpenGL：
+
+    sudo easy_install-2.7 pyserial  
+    sudo easy_install-2.7 PyOpenGL  
+
+You will need to download the appropriate wxPython Installer package and install it.  It is available from:  
+您需要下载相应的wxPython安装包并进行安装。下载地址：
+
+    http://www.wxpython.org/download.php  
+
+Specifically, install "wxPython2.9-osx-cocoa-py2.7" as it is the build that supports 64-bit execution.  
+请特别安装 "wxPython2.9-osx-cocoa-py2.7"，因为它支持64位执行。
+
+=======  
+RUNNING  
+运行  
+=======  
+
+Windows  
+-------
+
+Double-click skeinforge.bat and Printrun.bat.  
+双击 skeinforge.bat 和 Printrun.bat。
+
+Mac OS X & Linux  
+----------------
+
+Once built, the two apps -- Pronterface and Cura -- must be started from the command line (for now):  
+一旦构建完成，这两个应用程序——Pronterface 和 Cura——必须从命令行启动（目前是这样）：
+
+    # open a new terminal window and....  
+    打开一个新的终端窗口并执行以下命令....
+    cd osx64-Cura-NewUI-Beta4  
+    ./pronterface.sh &  
+    ./Cura.sh &  
+
+This will start both applications with their console logging output directed into the terminal window.  
+这将启动两个应用程序，并将它们的控制台日志输出重定向到终端窗口。
+
+========  
+FIRMWARE  
+固件  
+========  
+
+For Ultimaker users, it is highly recommended -- nearly required -- that you upgrade your firmware to the latest Marlin builds.  See:  
+对于Ultimaker用户，强烈建议——几乎是必须的——将固件升级到最新的Marlin版本。请参见：
+
+    http://wiki.ultimaker.com/Skeinforge_PyPy
---
+++ package.sh
@@ -0,0 +1,175 @@
+#!/bin/bash
+
+# This script is to package the Cura package for Windows/Linux and OSx
+# This script should run under Linux and OSx, as well as Windows with Cygwin.
+
+#############################
+# CONFIGURATION
+#############################
+
+
+##Select the build target
+BUILD_TARGET=${1:-win32}
+#BUILD_TARGET=linux
+#BUILD_TARGET=osx64
+
+##Do we need to create the final archive
+ARCHIVE_FOR_DISTRIBUTION=1
+##Which version name are we appending to the final archive
+BUILD_NAME=RC1
+TARGET_DIR=${BUILD_TARGET}-Cura-${BUILD_NAME}
+
+##Which versions of external programs to use
+PYPY_VERSION=c-jit-latest
+WIN_PORTABLE_PY_VERSION=2.7.2.1
+WIN_PYSERIAL_VERSION=2.5
+
+#############################
+# Support functions
+#############################
+function checkTool
+{
+	if [ -z `which $1` ]; then
+		echo "The $1 command must be somewhere in your \$PATH."
+		echo "Fix your \$PATH or install $2"
+		exit 1
+	fi
+}
+
+#############################
+# Actual build script
+#############################
+
+checkTool git "git: http://git-scm.com/"
+checkTool curl "curl: http://curl.haxx.se/"
+if [ $BUILD_TARGET = "win32" ]; then
+	#Check if we have 7zip, needed to extract and packup a bunch of packages for windows.
+	checkTool 7z "7zip: http://www.7-zip.org/"
+fi
+#For building under MacOS we need gnutar instead of tar
+if [ -z `which gnutar` ]; then
+	TAR=tar
+else
+	TAR=gnutar
+fi
+
+#############################
+# Download all needed files.
+#############################
+
+if [ $BUILD_TARGET = "win32" ]; then
+	#Get portable python for windows and extract it. (Linux and Mac need to install python themselfs)
+	if [ ! -f "PortablePython_${WIN_PORTABLE_PY_VERSION}.exe" ]; then
+		curl -L -O http://ftp.nluug.nl/languages/python/portablepython/v2.7/PortablePython_${WIN_PORTABLE_PY_VERSION}.exe
+	fi
+	if [ ! -f pyserial-${WIN_PYSERIAL_VERSION}.exe ]; then
+		curl -L -O http://sourceforge.net/projects/pyserial/files/pyserial/${WIN_PYSERIAL_VERSION}/pyserial-${WIN_PYSERIAL_VERSION}.win32.exe/download
+		mv download pyserial-${WIN_PYSERIAL_VERSION}.exe
+	fi
+	if [ ! -f PyOpenGL-3.0.1.win32.exe ]; then
+		curl -L -O http://sourceforge.net/projects/pyopengl/files/PyOpenGL/3.0.1/PyOpenGL-3.0.1.win32.exe
+	fi
+	#Get pypy
+	if [ ! -f "pypy-${PYPY_VERSION}-win32.zip" ]; then
+	#	curl -L -O https://bitbucket.org/pypy/pypy/downloads/pypy-${PYPY_VERSION}-win32.zip
+		curl -L -O http://buildbot.pypy.org/nightly/trunk/pypy-${PYPY_VERSION}-win32.zip
+	fi
+else
+	if [ ! -f "pypy-${PYPY_VERSION}-${BUILD_TARGET}.tar.bz2" ]; then
+	#	curl -L -O https://bitbucket.org/pypy/pypy/downloads/pypy-${PYPY_VERSION}-${BUILD_TARGET}.tar.bz2
+		curl -L -O http://buildbot.pypy.org/nightly/trunk/pypy-${PYPY_VERSION}-${BUILD_TARGET}.tar.bz2
+	fi
+fi
+
+#Get our own version of Printrun
+if [ ! -d "Printrun" ]; then
+  git clone git://github.com/daid/Printrun.git
+else
+  echo "Updating Printrun"
+  cd Printrun
+  git pull
+  cd ..
+fi
+
+#############################
+# Build the packages
+#############################
+rm -rf ${TARGET_DIR}
+mkdir -p ${TARGET_DIR}
+
+if [ $BUILD_TARGET = "win32" ]; then
+	#For windows extract portable python to include it.
+	7z x PortablePython_${WIN_PORTABLE_PY_VERSION}.exe \$_OUTDIR/App
+	7z x PortablePython_${WIN_PORTABLE_PY_VERSION}.exe \$_OUTDIR/Lib/site-packages
+	7z x pyserial-${WIN_PYSERIAL_VERSION}.exe PURELIB
+	7z x PyOpenGL-3.0.1.win32.exe PURELIB
+
+	mkdir -p ${TARGET_DIR}/python
+	mv \$_OUTDIR/App/* ${TARGET_DIR}/python
+	mv \$_OUTDIR/Lib/site-packages/wx* ${TARGET_DIR}/python/Lib/site-packages/
+	mv PURELIB/serial ${TARGET_DIR}/python/Lib
+	mv PURELIB/OpenGL ${TARGET_DIR}/python/Lib
+	rm -rf \$_OUTDIR
+	rm -rf PURELIB
+	
+	#Clean up portable python a bit, to keep the package size down.
+	rm -rf ${TARGET_DIR}/python/PyScripter.*
+	rm -rf ${TARGET_DIR}/python/Doc
+	rm -rf ${TARGET_DIR}/python/locale
+	rm -rf ${TARGET_DIR}/python/tcl
+	rm -rf ${TARGET_DIR}/python/Lib/test
+	rm -rf ${TARGET_DIR}/python/Lib/distutils
+	rm -rf ${TARGET_DIR}/python/Lib/site-packages/wx-2.8-msw-unicode/wx/tools
+	rm -rf ${TARGET_DIR}/python/Lib/site-packages/wx-2.8-msw-unicode/wx/locale
+	#Remove the gle files because they require MSVCR71.dll, which is not included. We also don't need gle, so it's safe to remove it.
+	rm -rf ${TARGET_DIR}/python/Lib/OpenGL/DLLS/gle*
+fi
+
+#Extract pypy
+if [ $BUILD_TARGET = "win32" ]; then
+	7z x pypy-${PYPY_VERSION}-win32.zip -o${TARGET_DIR}
+else
+	cd ${TARGET_DIR}; $TAR -xjf ../pypy-${PYPY_VERSION}-${BUILD_TARGET}.tar.bz2; cd ..
+fi
+mv ${TARGET_DIR}/pypy-*-${BUILD_TARGET} ${TARGET_DIR}/pypy
+#Cleanup pypy
+rm -rf ${TARGET_DIR}/pypy/lib-python/2.7/test
+
+#add Skeinforge
+cp -a Cura ${TARGET_DIR}/Cura
+
+#add printrun
+cp -a Printrun ${TARGET_DIR}/Printrun
+rm -rf ${TARGET_DIR}/Printrun/.git*
+
+#add script files
+if [ $BUILD_TARGET = "win32" ]; then
+    cp -a scripts/${BUILD_TARGET}/*.bat $TARGET_DIR/
+else
+    cp -a scripts/${BUILD_TARGET}/*.sh $TARGET_DIR/
+fi
+
+#add readme file
+cp README ${TARGET_DIR}/README.txt
+
+#package the result
+if (( ${ARCHIVE_FOR_DISTRIBUTION} )); then
+	if [ $BUILD_TARGET = "win32" ]; then
+		rm ${TARGET_DIR}.zip
+		cd ${TARGET_DIR}
+		7z a ../${TARGET_DIR}.zip *
+		cd ..
+		
+		if [ ! -z `which wine` ]; then
+			#if we have wine, try to run our nsis script.
+			ln -sf `pwd`/${TARGET_DIR} scripts/win32/dist
+			wine ~/.wine/drive_c/Program\ Files/NSIS/makensis.exe /DVERSION=${BUILD_NAME} scripts/win32/installer.nsi 
+		fi
+	else
+		echo "Archiving to ${TARGET_DIR}.tar.gz"
+		$TAR cfp - ${TARGET_DIR} | gzip --best -c > ${TARGET_DIR}.tar.gz
+	fi
+else
+	echo "Installed into ${TARGET_DIR}"
+fi
+
